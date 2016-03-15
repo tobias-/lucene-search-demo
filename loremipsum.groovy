34400bb073271fb259b01680f1882d82ca91591f
Tobias <tobias@olsson.be>
2016-03-15 23:27:00 +0100

    Less wordy

--- loremipsum.groovy
+++ loremipsum.groovy
@@ -4,7 +4,7 @@ import de.svenjacobs.loremipsum.LoremIpsum
 @Grab('de.sven-jacobs:loremipsum:1.0')
 def random = new Random()
 
-def words = random.nextInt(50);
+def words = random.nextInt(10);
 println(new LoremIpsum().getWords(words))
 if (random.nextBoolean()) {
     throw new Exception("This time we failed")
