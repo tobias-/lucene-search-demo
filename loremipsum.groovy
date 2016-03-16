1956b42eba7b845c4d2f6ba94e123ae8bb5070a8
Tobias <tobias@olsson.be>
2016-03-16 13:13:27 +0100

    20% chance of ssh error during "build"

--- loremipsum.groovy
+++ loremipsum.groovy
@@ -6,6 +6,10 @@ def random = new Random()
 
 def words = random.nextInt(50);
 println(new LoremIpsum().getWords(words))
+if (random.nextInt(5) == 0) {
+    throw new Exception("A undefined ssh error occured")
+}
+
 if (random.nextBoolean()) {
     throw new Exception("This time we failed")
 }
